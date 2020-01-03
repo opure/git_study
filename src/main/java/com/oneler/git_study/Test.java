06beb9bc5cbc48e6a03998fc9760448ff7412b4a
=gitee@aukeys.com <gitee@aukeysc.om>
2020-01-03 22:55:27 +0800

    dev1

--- src/main/java/com/oneler/git_study/Test.java
+++ src/main/java/com/oneler/git_study/Test.java
@@ -3,7 +3,7 @@ package com.oneler.git_study;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aa");
-        System.out.println("xxdev");
+        System.out.println("dev1");
         System.out.println("aa");
         System.out.println("aa");
         System.out.println("aa");
