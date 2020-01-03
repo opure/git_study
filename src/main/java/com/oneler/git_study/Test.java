a39475bb45484b84ea1c38ca37f3096ff75ca146
=gitee@aukeys.com <gitee@aukeysc.om>
2020-01-03 22:46:47 +0800

    3hang

--- src/main/java/com/oneler/git_study/Test.java
+++ src/main/java/com/oneler/git_study/Test.java
@@ -3,5 +3,7 @@ package com.oneler.git_study;
 public class Test {
     public static void main(String[] args) {
         System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
     }
 }
